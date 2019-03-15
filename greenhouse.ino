53bb23b760ac5e188b4b870f0ef82c2cc477d5d0
dasperg <dasperg@gmail.com>
2019-03-15 21:14:49 +0100

    Initial commit

---
+++ greenhouse.ino
@@ -0,0 +1,79 @@
+// Include the libraries we need
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into port 2 on the Arduino
+#define TEMPERATURE_PIN 8
+#define RELAY_OPEN_PIN 9
+#define RELAY_CLOSE_PIN 10
+
+#define OPEN_TEMP 30
+#define CLOSE_TEMP 25
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(TEMPERATURE_PIN);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
+/*
+   The setup function. We only start the sensors here
+*/
+void setup(void)
+{
+  // start serial port
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+
+  pinMode(RELAY_OPEN_PIN, OUTPUT);
+  pinMode(RELAY_CLOSE_PIN, OUTPUT);
+
+  digitalWrite(RELAY_OPEN_PIN, LOW);
+//  digitalWrite(RELAY_OPEN_PIN, HIGH);
+
+//  digitalWrite(RELAY_CLOSE_PIN, LOW);
+  digitalWrite(RELAY_CLOSE_PIN, HIGH);
+
+  // Start up the library
+  sensors.begin();
+}
+
+/*
+   Main function, get and show the temperature
+*/
+void loop(void)
+{
+  // call sensors.requestTemperatures() to issue a global temperature
+  // request to all devices on the bus
+  Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("DONE");
+  // After we got the temperatures, we can print them here.
+  // We use the function ByIndex, and as an example get the temperature from the first sensor only.
+  float tempC = sensors.getTempCByIndex(0);
+
+  // Check if reading was successful
+  if (tempC != DEVICE_DISCONNECTED_C)
+  {
+    Serial.print("Temperature for the device 1 (index 0) is: ");
+    Serial.println(tempC);
+
+//    // Open/Close logic
+//    if(tempC >= OPEN_TEMP) {
+//      digitalWrite(RELAY_OPEN_PIN, HIGH);
+//      digitalWrite(RELAY_CLOSE_PIN, LOW);
+//    } else if(tempC <= CLOSE_TEMP) {
+//      digitalWrite(RELAY_OPEN_PIN, LOW);
+//      digitalWrite(RELAY_CLOSE_PIN, HIGH);
+//    }
+  }
+  else
+  {
+    Serial.println("Error: Could not read temperature data");
+  }
+
+//  digitalWrite(RELAY_OPEN_PIN, HIGH);
+//  digitalWrite(RELAY_CLOSE_PIN, LOW);
+//  digitalWrite(RELAY_OPEN_PIN, LOW);
+//  digitalWrite(RELAY_CLOSE_PIN, HIGH);
+}
